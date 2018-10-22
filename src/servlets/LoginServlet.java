2b3c248788a68846bb992d216213ea0f4c0e0360
Timur Badretdinov <tim_baton@mail.ru>
2018-10-22 10:41:29 +0300

    Test

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 public class LoginServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
@@ -8,5 +10,7 @@ public class LoginServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.print("hello");
     }
 }
